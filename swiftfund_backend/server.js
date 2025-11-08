5e48025bd0987f86ead517061498309aac5bbc46
Micheal Oloyede <129639533+Oloyede-Michael@users.noreply.github.com>
2025-11-09 00:11:56 +0100

    Update server.js
    added Cors url

--- swiftfund_backend/server.js
+++ swiftfund_backend/server.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 // Enable CORS for your frontend URL
 const allowedOrigins = [
   "https://swift-funds.vercel.app",
+  "https://hedfunds.vercel.app/",
   "http://localhost:5173", 
   "http://localhost:5174",
   process.env.FRONTEND_URL
@@ -109,4 +110,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
 });
